d6f3f06514cf8ecb9c1d77355a8636544d9f816f
Jacey Davis <jaceyd923@gmail.com>
2020-04-23 01:40:06 -0600

    added GSAP animations

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # GSAP Project DGM 2780
 
-The URL used for the layout: 
+The URL used for the layout: https://www.minimalistmagnolia.com/
 
 URL to the completed website: 
+
+## GSAP ANIMATIONS
+* Hero image text on page load on the home page
+* Bike product cards on page load on the home (under Staff Picks) and shop pages
+* White circle buttons and product picture under Product Features on the home page
---
+++ website/js/index.js
@@ -0,0 +1,62 @@
+// HERO IMAGE TITLE ANIMATION
+const tl = new TimelineMax()
+
+tl.from(".hero-title", .6, {
+    opacity: 0,
+    y: -24,
+    ease: Expo.easeInOut
+}).from('.hero-button', .1, {
+    opacity: 0,
+    x: -40,
+    ease: Expo.easeInOut
+});
+
+// SHOP BIKES ANIMATION
+gsap.from(".bike-card", {
+    duration: 1.5,
+    scale: 0.5, 
+    opacity: 0, 
+    delay: 0.5, 
+    stagger: 0.2,
+    ease: "elastic", 
+});
+
+
+// SHOP THIS LOOK ANIMATIONS
+gsap.to(".circle-outline", 1, {
+    scale: 0.9, 
+    yoyo: true, 
+    repeat: -1, 
+    ease: "power1.inOut",
+});
+
+let circleIndicators = document.querySelectorAll('.circle-indicator');
+let productPicture = document.querySelector('.product-picture');
+let productTitle = document.querySelector('.product-title');
+
+circleIndicators.forEach(indicator => {
+    indicator.addEventListener('click', event => {
+        if (indicator.id === "indicator1") {
+            productPicture.src = "images/wheel.jpg";
+            productTitle.innerHTML = "PREMIUM WHEELS <span>$60</span>";
+        } else if (indicator.id === "indicator2") {
+            productPicture.src = "images/helmet.jpg";
+            productTitle.innerHTML = "BIKE HELMET <span>$35</span>";
+        } else {
+            productPicture.src = "images/seat.jpg";
+            productTitle.innerHTML = "BIKE SEAT <span>$25</span>";
+        }
+
+        gsap.from(".product-picture", {
+            duration: 0.5, 
+            opacity: 0, 
+            y: -100, 
+            ease: "back.out"
+        });
+    })
+
+})
+
+
+
+
